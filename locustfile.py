da5c490a79b310012de5a02711f20a3d7ecb920a
migyeong <wjdalrud5516@gmail.com>
2025-09-22 16:21:48 +0900

    비회원 시나리오 추가 및 profile 예시

--- locustfile.py
+++ locustfile.py
@@ -1,30 +1,47 @@
 from locust import HttpUser, task, between, SequentialTaskSet
 
+# 비회원 시나리오
 class GuestUser(HttpUser):
-    # 일반 방문자 역할
     wait_time = between(1, 3)
 
-    @task(3)  # 가중치 3: 가장 자주 실행되는 작업
-    def view_news(self):
-        self.client.get("/news", name="뉴스 페이지 조회")
-
-    @task(1)  # 가중치 1: 상대적으로 덜 자주 실행
-    def view_info(self):
-        self.client.get("/info", name="선수 및 구단 정보 조회")
-
-class RegisteredUser(HttpUser):
-    # 로그인한 사용자 역할
-    wait_time = between(2, 5)
-
-    @task
-    def login(self):
-        # 로그인 로직을 여기에 구현
-        pass
-
+    # 비회원 뉴스 페이지 조회 시나리오
     @task(2)
-    def my_page(self):
-        self.client.get("/mypage", name="마이페이지 조회")
+    def view_news(self):
+        self.client.get("/api/news/", name="비회원 뉴스 페이지 조회")
 
-    @task(1)
-    def participate_event(self):
-        self.client.post("/event/participate", name="이벤트 참여")
+    # 비회원 게시판 페이지 조회 시나리오
+    @task(3)
+    def view_board(self):
+        self.client.get("/api/board/posts/", name="비회원 게시판 페이지 조회")
+
+    # 비회원 경기 일정 조회 시나리오
+    @task(2)
+    def view_event_schedule(self):
+        self.client.get("/api/event/schedules/", name="비회원 경기 일정 조회")
+
+    # 비회원 이벤트 참여 페이지 조회 시나리오
+    @task(3)
+    def view_event_part(self):
+        self.client.get("/api/event/participate/", name="비회원 이벤트 참여 페이지 조회")
+
+    # @task(3)
+    # def go_signup(self):
+    #     self.client.post("/api/user/signup/", name="비회원 회원가입 페이지 이동")
+
+# 회원 시나리오
+# class RegisteredUser(HttpUser):
+#     # 로그인한 사용자 역할
+#     wait_time = between(2, 5)
+
+#     @task
+#     def login(self):
+#         # 로그인 로직을 여기에 구현
+#         pass
+
+#     @task(2)
+#     def my_page(self):
+#         self.client.get("/mypage", name="마이페이지 조회")
+
+#     @task(1)
+#     def participate_event(self):
+#         self.client.post("/event/participate", name="이벤트 참여")
